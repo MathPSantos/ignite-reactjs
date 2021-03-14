2ff1ef681cff1378da746e641e10dc69702804b0
Matheus Santos <matheus.santos@intelitrader.com.br>
2021-03-14 07:36:22 -0300

    01: add typescript to project

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export function RepositoryItem(props) {
       <strong>{props.repository?.name ?? 'Default'}</strong>
       <p>{props.repository?.description ?? 'Default desc'}</p>
 
-      <a href={props.repository?.link}>
+      <a href={props.repository?.html_url}>
         Acessar repositórios
       </a>
     </li>
--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,22 +1,24 @@
-import { RepositoryItem } from "./RepositoryItem";
+import { useEffect, useState } from 'react'
+import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
-const repository = {
-  name: 'unform',
-  description: "Forms in React", 
-  link: "https://github.com"
-}
-
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+  
+  useEffect(() => {
+    fetch('https://api.github.com/users/MathPSantos/repos')
+      .then(res => res.json())
+      .then(data => setRepositories(data))
+      .catch(error => console.log(error))
+  }, [])
+
   return(
     <section className="repository-list">
       <h1>Lista de repositrórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        { repositories.map((e, i) => <RepositoryItem key={i} repository={e} />) }
       </ul>
     </section>
   )
